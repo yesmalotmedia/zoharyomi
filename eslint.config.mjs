0d33767209816303fb48e830a14677ffe248588a
yesmalotmedia <media@yesmalot.co.il>
2025-12-09 09:38:01 +0200

    fix: update Next.js due to CVE security issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,25 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import { defineConfig } from "eslint/config";
+import nextPlugin from "eslint-plugin-next";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+export default defineConfig([
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
+
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+
+    plugins: {
+      next: nextPlugin,
+    },
+
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+    },
+  },
 ]);
-
-export default eslintConfig;
